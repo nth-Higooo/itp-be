0ab4dc85ddd68802e14c8a11eead1316deb9a317
nth-Higooo <nth.hiennguyenthanh@gmail.com>
2025-05-07 00:58:11 +0700

    migrate-fix

--- src/database/migrations/1722307378376-init-user.ts
+++ src/database/migrations/1722307378376-init-user.ts
@@ -5,21 +5,21 @@ export class InitData1722307378376 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO users ("displayName", email, "hashPassword", status, "createdBy")
-            SELECT 'Man Tran', 'man.tran@watasoftware.com', '${getHashPassword(
-              "123456@Wts"
+            SELECT 'Hien Nguyen', 'nth.hiennguyenthanh@gmail.com', '${getHashPassword(
+              "12345678@"
             )}', 'ACTIVE', 'migration'
             WHERE NOT EXISTS (
                 SELECT 1
                 FROM users
-                WHERE email = 'man.tran@watasoftware.com'
+                WHERE email = 'nth.hiennguyenthanh@gmail.com'
             );`
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DELETE FROM users WHERE email='man.tran@watasoftware.com';
-      DELETE FROM sessions WHERE email='man.tran@watasoftware.com';`
+      `DELETE FROM users WHERE email='nth.hiennguyenthanh@gmail.com';
+      DELETE FROM sessions WHERE email='nth.hiennguyenthanh@gmail.com';`
     );
   }
 }
